957c3bda7f5af9bdc16e5ae927f2f35090560caf
Tiago Oliveira <tiago.oliveira.tech@outlook.com>
2026-06-14 13:05:02 +0000

    Primeiro commit - exercícios Java

--- ex01-tabuada/Tabuada.java
+++ ex01-tabuada/Tabuada.java
@@ -2,7 +2,16 @@ import java.util.Scanner;
 
 public class Tabuada {
     public static void main(String[] args) {
-        
+         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número para ver a tabuada: ");
+        int numero = sc.nextInt();
+
+
+        System.out.println("Tabuada do " + numero + ":");
+        for (int i = 1; i <= 10; i++) {
+            
+            System.out.println(numero + " x " + i + " = " + (numero *
 
 
 
