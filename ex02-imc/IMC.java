3990bc5ddef4d4faf74db36134ef65e2e6b63a95
Tiago Oliveira <tiago.oliveira.tech@outlook.com>
2026-06-14 14:13:36 +0000

    Adiciona cálculo de IMC com Scanner

--- ex02-imc/IMC.java
+++ ex02-imc/IMC.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
 
+
+
+public class IMC {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     
+
+        System.out.print("Digite sua altura (ex: 1.75): ");
+        double altura = sc.nextDouble();
+
+       System.out.print("Digite seu peso (ex: 70.5): ");
+        double peso = sc.nextDouble(); 
+     
+       double imc = peso / (altura * altura);
+
+        System.out.printf("\nSeu IMC é: %.2f%n", imc);
+     
+     
+     
+     
+      }
+
+
+
+
+
+
+
+     
+      }
+
+
+
+
+
+
+
+
+
+}
