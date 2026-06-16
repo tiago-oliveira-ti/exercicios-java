723e1d5c06ed43275d68400e3c09b50e8a8ce8e1
Tiago Oliveira <tiago.oliveira.tech@outlook.com>
2026-06-16 14:26:13 +0000

    Refatoração: código IMC mais limpo e organizado

--- src/ex02_imc/IMC.java
+++ src/ex02_imc/IMC.java
@@ -1,23 +1,18 @@
 package ex02_imc;
 
-
 import java.util.Scanner;
 
-
-
 public class IMC {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-     
 
         System.out.print("Digite sua altura (ex: 1.75): ");
         double altura = sc.nextDouble();
 
-       System.out.print("Digite seu peso (ex: 70.5): ");
-        double peso = sc.nextDouble(); 
-     
-       double imc = peso / (altura * altura);
+        System.out.print("Digite seu peso (ex: 70.5): ");
+        double peso = sc.nextDouble();
 
+        double imc = peso / (altura * altura);
         System.out.printf("\nSeu IMC é: %.2f%n", imc);
 
         if (imc <= 18.5) {
@@ -35,17 +30,8 @@ public class IMC {
         }
 
         sc.close();
-     
-     
-     
-     
-      }
-
-
-
-
-      }
-
+    }
+}
 
 
 
